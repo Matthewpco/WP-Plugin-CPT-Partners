bab9c6dd8935605a48e06bc6e3754f36633e75b5
Matthew Payne <matthewpco86@gmail.com>
2021-12-28 18:59:01 -0700

    update v1.01
    added clear rewrite rules on activation and deactivation

---
+++ README.md
@@ -0,0 +1,26 @@
+![](https://raw.githubusercontent.com/Matthewpco/Jubilee-Design/master/img/JD-SS1.png)
+
+# This is a custom plugin designed for a complex e-commerce website that sells custom built computers. 
+
+<br>
+
+
+## 🙋‍♂️ Introduction 
+
+- This WordPress plugin extends funtionality by adding new post types and custom fields to dynamically generate partner page data saving the space of hudnreds of pages that had to be created and maintained individually. Can also be used with Woo Commerce to dynamically generate custom composite products instead of having multiple individual products.
+
+
+<br>
+
+## 📜 Features
+- WordPress
+- Plugin Development
+- PHP
+<br>
+
+
+## ⚡ Changelog for v 1.01
+- Added clear rewrite rules upon activation and deactivation
+- Fixed a bug with permalinks not updating immediately
+<br>
+![](https://raw.githubusercontent.com/Matthewpco/Jubilee-Design/master/img/JD-SS2.png)
--- artesian-partners-cpt.php
+++ artesian-partners-cpt.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package artesian-partners-cpt
- * @version 1.0
+ * @version 1.1
  */
 /*
 Plugin Name: Artesian Partners CPT
 Plugin URI: http://wpwebdevelopment.com
 Description: Establishes custom post types for different level of partners to display their information as well as setting up a post type linked to custom woocommerce product combinations
 Author: Matthew Payne
-Version: 1.0
+Version: 1.1
 Author URI: http://wpwebdevelopment.com
 */
 // Exit if accessed directly.
@@ -428,3 +428,20 @@ function register_partner_fields() {
 // Run functions on plugin init to register post types and fields
 add_action( 'init', 'register_partners_cpt' );
 add_action( 'init', 'register_partner_fields' );
+
+// Flush rewrites on activation
+function apcpt_activation() {
+    
+        register_partners_cpt();
+        flush_rewrite_rules();
+    }
+    
+    register_activation_hook( __FILE__, 'apcpt_activation' );
+    
+    // Flush our rewrite rules on deactivation.
+     
+    function apcpt_deactivation() {
+    
+        flush_rewrite_rules();
+    }
+register_deactivation_hook( __FILE__, 'apcpt_deactivation' );
